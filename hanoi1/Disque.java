6355830c4baa7c62e1f3aa11522975b904ff40cd
Philippe Claude <pclaud1@a-018818.ups.u-psud.fr>
2018-10-17 15:12:48 +0200

    Q2 finish

--- hanoi1/Disque.java
+++ hanoi1/Disque.java
@@ -1,18 +1,27 @@
-public class Disque implements Comparable {
-    private int val;
+public class Disque implements Empilable {
+    private int taille;
+	
+    public Disque () {
+    	
+    }
 
     public Disque (int v) {
-	val = v;
+	init(v);
     }
 
     public String toString() {
-	return "Disque[" + val + "]";
+	return "Disque[" + taille + "]";
     }
 
     public int compareTo(Object v) {
 	Disque d = (Disque) v;
-	if (val < d.val) return -1;
-	else if (val == d.val) return 0;
+	if (taille < d.taille) return -1;
+	else if (taille == d.taille) return 0;
 	else return 1;
+    }
+    
+    public void init(int taille)
+    {
+    	this.taille = taille;
     }
 }
